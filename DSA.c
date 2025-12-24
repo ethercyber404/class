099a2aa4357245c9356864a96e1390e56030a141
E-T-H-E-R <ether.cybe@gmail.com>
2025-12-24 08:55:40 -0800

    Update DSA.c

--- DSA.c
+++ DSA.c
@@ -1,72 +1,71 @@
 //1. Define a struct Student with id (int) and cgpa (float). Create one variable, assign values, and print them.
+ #include <stdio.h>                 
+ #include <stdlib.h>
 
-// #include <stdio.h>                 
-// #include <stdlib.h>
+int main() 
+{
+    struct st 
+    {
+        int id;
+        float cg;
+    };
 
-// int main() 
-// {
-//     struct st 
-//     {
-//         int id;
-//         float cg;
-//     };
+    struct st st_1;
+    st_1.id = 48;
+    st_1.cg = 3.29;
 
-//     struct st st_1;
-//     st_1.id = 48;
-//     st_1.cg = 3.29;
+    printf("Student ID : %d\nCGPA : %.2f",st_1.id,st_1.cg);
 
-//     printf("Student ID : %d\nCGPA : %.2f",st_1.id,st_1.cg);
-
-//     return 0;
-// }
+    return 0;
+}
 
 //2. Create a struct Point with x and y. Ask the user for input and calculate the distance from the origin (0,0).
 
-// #include <stdio.h>
-// #include <math.h> 
+#include <stdio.h>
+#include <math.h> 
 
-// int main() {
+int main() {
 
-//     struct dis {
-//     float x,y,distance;
-//     };
-//     struct dis a;
+    struct dis {
+    float x,y,distance;
+    };
+    struct dis a;
 
-//     printf("Enter the value of x : ");
-//     scanf("%f", &a.x);
+    printf("Enter the value of x : ");
+    scanf("%f", &a.x);
 
-//     printf("Enter the value of y : ");
-//     scanf("%f", &a.y);
+    printf("Enter the value of y : ");
+    scanf("%f", &a.y);
 
-//     a.distance = sqrt((a.x * a.x)+(a.y * a.y));
+    a.distance = sqrt((a.x * a.x)+(a.y * a.y));
 
-//     printf("The distance will be: %.2f",a.distance);
+    printf("The distance will be: %.2f",a.distance);
 
-//     return 0;
-// }
+    return 0;
+}
 
 //3. Create a struct variable and a pointer to that variable. Access members using both the . operator and the -> operator.
 
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// int main() {
+int main() {
 
-//     struct St {
-//     int id;
-//     float cg;
-//     };
-//     struct St st_1;     
-//     struct St *loc; 
+    struct St {
+    int id;
+    float cg;
+    };
+    struct St st_1;     
+    struct St *loc; 
 
-//     st_1.id = 48;
-//     st_1.cg = 3.29;
+    st_1.id = 48;
+    st_1.cg = 3.29;
 
-//     loc = &st_1;   
-//     printf("ID : %d\nCGPA : %.2f",loc->id,loc->cg);
+    loc = &st_1;   
+    printf("ID : %d\nCGPA : %.2f",loc->id,loc->cg);
 
-//     return 0;
-// }
+    return 0;
+}
 
 //4. Create an array of 3 Product structs (name, price). Use a loop to calculate the total price of all products.
 
